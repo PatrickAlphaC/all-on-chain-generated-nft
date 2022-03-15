e19aca3b978e094542d32947a4caafc88d188513
mattjaf <mattjaf@gmail.com>
2022-03-15 03:08:03 -0400

    updated hardhat-config.js
    fixed the compiler to 8.1, commented out private keys, and filled in the " "'s

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,104 +1,106 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ethers");
-require("@nomiclabs/hardhat-truffle5");
-require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-ethers")
+require("@nomiclabs/hardhat-truffle5")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
 
-require("dotenv").config();
 
-const MAINNET_RPC_URL =
-    process.env.MAINNET_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || "";
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "";
-const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "";
-const MNEMONIC = process.env.MNEMONIC || "";
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+require('dotenv').config()
+
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || "https://eth-mainnet.alchemyapi.io/v2/..........your key"
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://rinkeby.infura.io/v3/.....................your key"
+const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://kovan.infura.io/v3/......your key"
+const MNEMONIC = process.env.MNEMONIC || "your mnemonic"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "your api key"
 // optional
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "your private key"
 
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
+
             // // If you want to do some forking, uncomment this
             // forking: {
             //   url: MAINNET_RPC_URL
             // }
         },
-        localhost: {},
+        localhost: {
+        },
         kovan: {
             url: KOVAN_RPC_URL,
-            accounts: [PRIVATE_KEY],
-            // accounts: {
-            //     mnemonic: MNEMONIC,
-            // },
+            // accounts: [PRIVATE_KEY],
+            accounts: {
+                mnemonic: MNEMONIC,
+            },
             saveDeployments: true,
         },
         rinkeby: {
             url: RINKEBY_RPC_URL,
-            accounts: [PRIVATE_KEY],
+            //  accounts: [PRIVATE_KEY],
             // accounts: {
-            //     mnemonic: MNEMONIC,
+            //  mnemonic: MNEMONIC,
             // },
             saveDeployments: true,
         },
         ganache: {
-            url: "http://localhost:8545",
-            accounts: [PRIVATE_KEY],
-            // accounts: {
-            //     mnemonic: MNEMONIC,
-            // }
+            url: 'http://localhost:8545',
+            accounts: {
+                mnemonic: MNEMONIC,
+            }
         },
         mainnet: {
             url: MAINNET_RPC_URL,
-            accounts: [PRIVATE_KEY],
-            // accounts: {
-            //     mnemonic: MNEMONIC,
-            // },
+            // accounts: [PRIVATE_KEY],
+            accounts: {
+                mnemonic: MNEMONIC,
+            },
             saveDeployments: true,
         },
         polygon: {
             url: "https://rpc-mainnet.maticvigil.com/",
-            accounts: [PRIVATE_KEY],
-            // accounts: {
-            //     mnemonic: MNEMONIC,
-            // },
+            // accounts: [PRIVATE_KEY],
+            accounts: {
+                mnemonic: MNEMONIC,
+            },
             saveDeployments: true,
         },
     },
     etherscan: {
         // Your API key for Etherscan
         // Obtain one at https://etherscan.io/
-        apiKey: ETHERSCAN_API_KEY,
+        apiKey: ETHERSCAN_API_KEY
     },
     namedAccounts: {
         deployer: {
             default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+            1: 0 // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
         },
         feeCollector: {
-            default: 1,
-        },
+            default: 1
+        }
+
     },
     solidity: {
         compilers: [
             {
-                version: "0.8.4",
+                version: "0.8.1"
             },
             {
-                version: "0.7.0",
+                version: "0.7.0"
             },
             {
-                version: "0.6.6",
+                version: "0.6.6"
             },
             {
-                version: "0.4.24",
-            },
-        ],
+                version: "0.4.24"
+            }
+        ]
     },
     mocha: {
-        timeout: 100000,
-    },
-};
+        timeout: 100000
+    }
+}
